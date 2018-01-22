1686da5f1bb809e6fe60945783305242edaadcd8
sjurasik <sjurasik@gmail.com>
2018-01-22 23:06:40 +0100

    Variables

---
+++ HelloWorld.py
@@ -0,0 +1,32 @@
+# greeting = "Hello"
+# name = input("please imput your name ")
+# print(greeting + " " + name)
+#
+# splitstring = "The string has been\nsplit over\nacros several\nlines"
+# print(splitstring)
+#
+# tabbedstring = "1\t2\t3\t4\t"
+# print(tabbedstring)
+
+
+# name = "slawek"
+# age = 24
+
+#Nie mozna uzywac zmiennych roznych typow w jednej funkcji
+# print(name + ' ' + age)
+
+#Integer
+n = 20
+t = 5
+
+
+print(n + t)
+print(n * t)
+
+# dzielenie daje float
+print(n / t)
+
+# dzielenie z dwoam slashami daje int
+print(n // t)
+print(n % t)
+
